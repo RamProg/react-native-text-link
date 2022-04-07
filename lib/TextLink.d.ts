382159c981dd4c3c2da61bf3646f7a80ec3856ae
Ramiro de la Peña <ramiro@whitespectre.com>
2022-04-07 10:08:33 -0300

    testing husky

--- lib/TextLink.d.ts
+++ lib/TextLink.d.ts
@@ -1,21 +1,15 @@
 /// <reference types="react" />
 import { StyleProp, TextStyle } from 'react-native';
 declare type LinkType = {
-  text: string;
-  onPress: () => void;
+    text: string;
+    onPress: () => void;
 };
 declare type TextLinkPropsType = {
-  links: LinkType[];
-  children: string;
-  textStyle?: StyleProp<TextStyle>;
-  textLinkStyle?: StyleProp<TextStyle>;
-  pressingLinkStyle?: StyleProp<TextStyle>;
+    links: LinkType[];
+    children: string;
+    textStyle?: StyleProp<TextStyle>;
+    textLinkStyle?: StyleProp<TextStyle>;
+    pressingLinkStyle?: StyleProp<TextStyle>;
 };
-declare const TextLink: ({
-  links,
-  children,
-  textStyle,
-  textLinkStyle,
-  pressingLinkStyle,
-}: TextLinkPropsType) => JSX.Element;
+declare const TextLink: ({ links, children, textStyle, textLinkStyle, pressingLinkStyle, }: TextLinkPropsType) => JSX.Element;
 export default TextLink;
